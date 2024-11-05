88cf6bbc4ae0372014057667a8e2d5bad708b229
Larss830 <larence830@gmail.com>
2024-11-05 18:55:26 +0800

    add scroll js functions

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -23,18 +23,18 @@ CShamburgerMenu.addEventListener('click', function() {
 //     }
 // }
 
-    // This script adds a class to the body after scrolling 100px
+//     This script adds a class to the body after scrolling 100px
 // and we used these body.scroll styles to create some on scroll 
 // animations with the navbar
 
-// document.addEventListener('scroll', (e) => { 
-//     const scroll = document.documentElement.scrollTop;
-//     if(scroll >= 100){
-// document.querySelector('body').classList.add('scroll')
-//     } else {
-//     document.querySelector('body').classList.remove('scroll')
-//     }
-// });
+document.addEventListener('scroll', (e) => { 
+    const scroll = document.documentElement.scrollTop;
+    if(scroll >= 100){
+document.querySelector('body').classList.add('scroll')
+    } else {
+    document.querySelector('body').classList.remove('scroll')
+    }
+});
 
 
 // mobile nav toggle code
