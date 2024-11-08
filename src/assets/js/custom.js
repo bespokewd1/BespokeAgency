de48fcacccfcfbd6bea72c2123488a57a04c9ec3
Larss830 <larence830@gmail.com>
2024-11-09 04:15:51 +0800

    add domain checker availability section

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,5 +1,5 @@
 var typing=new Typed(".cs-changing-text", {
-    strings: ["Entrepreneurs", "Plumbers", "Contractors", "Small Businesses"],
+    strings: ["Entrepreneurs", "Plumbers", "Contractors", "Small Businesses", "Startups"],
     typeSpeed: 100,
     backSpeed: 40,
     loop: true,
