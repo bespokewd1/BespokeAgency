37d97852e17691a2a71ed415c7c2471b2ee4564f
Larss830 <larence830@gmail.com>
2024-11-09 03:48:33 +0800

    optimize typing header speed

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,7 +1,7 @@
 var typing=new Typed(".cs-changing-text", {
-    strings: ["", "Entrepreneurs", "Plumbers", "Contractors", "Small Businesses"],
+    strings: ["Entrepreneurs", "Plumbers", "Contractors", "Small Businesses"],
     typeSpeed: 100,
     backSpeed: 40,
     loop: true,
-    backDelay: 3000,
+    backDelay: 2000,
 });
