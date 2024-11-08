bf865146264a1fd7f2358032474513fca75d5c58
Larss830 <larence830@gmail.com>
2024-11-09 03:31:32 +0800

    change animation to typing

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,45 +1,7 @@
-var isLast = function(word) {
-    return word.nextElementSibling ? false : true;
-  }
-  
-  var getNext = function(word) {
-    return word.nextElementSibling;
-  }
-  
-  var getVisible = function() {
-    return document.getElementsByClassName('is-visible')[0]; // assuming only one element is visible at a time
-  }
-  
-  var getFirst = function() {
-    return document.querySelector('.words-wrapper').firstElementChild;
-  }
-  
-  var switchWords = function(current, next) {
-    current.classList.remove('is-visible');
-    current.classList.add('is-hidden');
-    next.classList.remove('is-hidden');
-    next.classList.add('is-visible');
-  }
-  
-  var getStarted = function() {
-    var first = getVisible();
-    var next = getNext(first);
-    
-    if (next) {
-      switchWords(first, next);
-    } else {
-      first.classList.remove('is-visible');
-      first.classList.add('is-hidden');
-      
-      var newEl = getFirst();
-      newEl.classList.remove('is-hidden');
-      newEl.classList.add('is-visible');
-    }
-  }
-  
-  var init = function() {
-    setInterval(getStarted, 3000);
-  }
-  
-  init();
-  
+var typing=new Typed(".cs-changing-text", {
+    strings: ["", "Entrepreneurs", "Plumbers", "Contractors", "Small Businesses"],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+    backDelay: 3000,
+});
